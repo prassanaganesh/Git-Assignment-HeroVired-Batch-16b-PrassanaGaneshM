6ba7ce226fb147c9f44f1252bec3cc19b5d24d66
prassanaganesh <prassana.ganesh@gmail.com>
2026-03-29 00:06:00 +0530

    added aquareroot feature

--- calculator.py
+++ calculator.py
@@ -1,10 +1,2 @@
-import math
-class Calculator:
-    def add(self, a, b):
-        return a + b
-    def subtract(self, a, b):
-        return a - b
-    def multiply(self, a, b):
-        return a * b
-    def divide(self, a, b):
-        return a / b
+def square_root(self, x):
+    return math.sqrt(x)
